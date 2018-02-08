e3c3089e9d6b6af701f140a766881014b9876094
Neven Sajko <nsajko@gmail.com>
2018-02-08 03:18:15 +0100

    Add simple digraph and monograph frequency counter tool

---
+++ digraphTest.c
@@ -0,0 +1,71 @@
+#include <ctype.h>  // isalnum
+#include <stdio.h>  // fputc
+#include <stdlib.h> // qsort
+
+#include "alphabet/alphabet.h"
+#include "buffer/buffer.h"
+#include "distance/distance.h"
+#include "sizeofMacros.h"
+
+static void
+brute(buffer *buf) {
+	static double digraphFreqs[alphabetSize][alphabetSize];
+	double freqs[alphabetSize];
+
+	monographFreqCount(buf, freqs);
+	printf("Monograph freq: %g\n", monographFreqScore(freqs, sumOfSquares));
+
+	digraphFreqCount(buf, digraphFreqs);
+	printf("Digraph freq: %g\n", digraphFreqScore(digraphFreqs, sumOfSquares));
+}
+
+static void
+pr(buffer *o) {
+	size_t i;
+	for (i = 0; i < o->l; i++) {
+		fputc(o->b[i], stdout);
+	}
+}
+
+static unsigned char stringBuf[1 << 25];
+
+static void
+f(void) {
+	for (;;) {
+		buffer si;
+		int i = 0;
+		int chr;
+		for (;;) {
+			chr = fgetc(stdin);
+			if (chr == EOF || !isalnum(chr)) {
+				break;
+			}
+			stringBuf[i] = chr;
+			i++;
+		}
+		for (; chr != EOF;) {
+			chr = fgetc(stdin);
+			if (isalnum(chr)) {
+				ungetc(chr, stdin);
+				break;
+			}
+		}
+		si.b = stringBuf;
+		si.l = i;
+
+		brute(&si);
+
+		pr(&si);
+		fputc('\n', stdout);
+
+		if (chr == EOF) {
+			break;
+		}
+	}
+}
+
+int
+main(void) {
+	f();
+	return 0;
+}
